3dcbccb0683469df30e2a579078701dbe024b9e5
leon mak <leonmakansheng@gmail.com>
2017-02-03 14:18:27 +0800

    Add method

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -13,6 +13,10 @@ public class CommandResult {
     /** The feedback message to be shown to the user. Contains a description of the execution result */
     private final String feedbackToUser;
 
+    public String getFeedbackToUser() {
+    	return feedbackToUser;
+    }
+    
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
 
