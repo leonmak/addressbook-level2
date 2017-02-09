0de0a7579d07a44ec5ca7e59128c49685a63b622
leon mak <leonmakansheng@gmail.com>
2017-02-10 00:52:15 +0800

    Add failing test

---
+++ test/java/seedu/addressbook/common/NameTest.java
@@ -0,0 +1,25 @@
+package seedu.addressbook.common;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.Name;
+
+public class NameTest {
+
+	@Test
+	public void isSimilar() throws IllegalValueException {
+		Name n1 = new Name("");
+		Name n2 = new Name("");
+		assertTrue(n1.isSimilar(n2));
+		
+	}
+
+}
