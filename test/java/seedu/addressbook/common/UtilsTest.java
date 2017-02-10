f30b3390f153a775f16ce3bf3f928a6af478c191
leon mak <leonmakansheng@gmail.com>
2017-02-10 16:14:25 +0800

    Refactor test to naming convention

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -13,51 +13,54 @@ import seedu.addressbook.data.person.Person;
 import seedu.addressbook.util.TestUtil;
 
 public class UtilsTest {
-    @Test
-    public void isAnyNull() {
-        // empty list
-        assertFalse(Utils.isAnyNull());
+//	[UnitOfWork__StateUnderTest__ExpectedBehavior]
+//  unit of work - public method return value/exception, or a state change, or call to a third party
 
-        // Any non-empty list
+    @Test
+    public void isAnyNull_EmptyList_False() {
+        assertFalse(Utils.isAnyNull());
+    }
+
+    @Test
+	public void isAnyNull_AnyNonEmptyList_False() {
         assertFalse(Utils.isAnyNull(new Object()));
         assertFalse(Utils.isAnyNull(new Object(), new Object()));
         assertFalse(Utils.isAnyNull("test"));
         assertFalse(Utils.isAnyNull(""));
-
-        // non empty list with just one null at the beginning
-        assertTrue(Utils.isAnyNull((Object) null));
-        assertTrue(Utils.isAnyNull(null, "", new Object()));
-        assertTrue(Utils.isAnyNull(null, new Object(), new Object()));
-
-        // non empty list with nulls in the middle
-        assertTrue(Utils.isAnyNull(new Object(), null, null, "test"));
-        assertTrue(Utils.isAnyNull("", null, new Object()));
-
-        // non empty list with one null as the last element
-        assertTrue(Utils.isAnyNull("", new Object(), null));
-        assertTrue(Utils.isAnyNull(new Object(), new Object(), null));
-
-        // confirms nulls inside the list are not considered
+	}
+    
+    @Test
+	public void isAnyNull_NullInsideList_False() {
+		// confirms nulls inside the list are not considered
         List<Object> nullList = Arrays.asList((Object) null);
         assertFalse(Utils.isAnyNull(nullList));
-    }
+	}
+    
+    @Test
+	public void isAnyNull_NonEmptyListNullLast_True() {
+		// non empty list with one null as the last element
+        assertTrue(Utils.isAnyNull("", new Object(), null));
+        assertTrue(Utils.isAnyNull(new Object(), new Object(), null));
+	}
+    
+    @Test
+	public void isAnyNull_NonEmptyListNullMidlle_True() {
+		// non empty list with nulls in the middle
+        assertTrue(Utils.isAnyNull(new Object(), null, null, "test"));
+        assertTrue(Utils.isAnyNull("", null, new Object()));
+	}
 
     @Test
-    public void elementsAreUnique() throws Exception {
-        // empty list
-        assertAreUnique();
-
-        // only one object
-        assertAreUnique((Object) null);
-        assertAreUnique(1);
-        assertAreUnique("");
-        assertAreUnique("abc");
-
-        // all objects unique
-        assertAreUnique("abc", "ab", "a");
-        assertAreUnique(1, 2);
-
-        // some identical objects
+	public void isAnyNull_NonEmptyListOneNullInBeginning_True() {
+        // non empty list with just one null at the beginning
+		assertTrue(Utils.isAnyNull((Object) null));
+        assertTrue(Utils.isAnyNull(null, "", new Object()));
+        assertTrue(Utils.isAnyNull(null, new Object(), new Object()));
+	}
+    
+    @Test
+	public void elementsAreUnique_someIdentical_False() throws Exception {
+		// some identical objects
         assertNotUnique("abc", "abc");
         assertNotUnique("abc", "", "abc", "ABC");
         assertNotUnique("", "abc", "a", "abc");
@@ -65,11 +68,29 @@ public class UtilsTest {
         assertNotUnique(null, 1, new Integer(1));
         assertNotUnique(null, null);
         assertNotUnique(null, "a", "b", null);
-        
-        // some test for person objects
-        assertAreUnique(TestUtil.generateTestPerson(), TestUtil.generateTestPerson());
-        assertNotUnique(Name.EXAMPLE, Name.EXAMPLE);
-    }
+	}
+    
+    @Test
+	public void elementsAreUnique_AllUnique_True() throws Exception {
+		// all objects unique
+        assertAreUnique("abc", "ab", "a");
+        assertAreUnique(1, 2);
+	}
+    
+    @Test
+	public void elementsAreUnique_OneObject_True() throws Exception {
+		// only one object
+        assertAreUnique((Object) null);
+        assertAreUnique(1);
+        assertAreUnique("");
+        assertAreUnique("abc");
+	}
+    
+    @Test
+	public void elementsAreUnique_EmptyList_True() throws Exception {
+		// empty list
+        assertAreUnique();
+	}
 
     private void assertAreUnique(Object... objects) {
         assertTrue(Utils.elementsAreUnique(Arrays.asList(objects)));
